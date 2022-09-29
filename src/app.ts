c345b020af5dec7b923e01a988eb995edccb3bc1
Lily Do <dothuthao1504@gmail.com>
2022-09-30 01:47:22 +0800

    implement update profile endpoint

--- src/app.ts
+++ src/app.ts
@@ -27,5 +27,29 @@ app.get('/candidate/:id', async (req, res) => {
   res.send({ data: dbRes.rows[0] || {}, status: 'success' });
 });
 
+// POST name, age, method route
+app.post('/candidate/:id', async (req, res) => {
+  const { name, age, profile_image_url, is_private } = req.body;
+  const { id } = req.params;
+  if (
+    name === undefined ||
+    age === undefined ||
+    profile_image_url === undefined ||
+    is_private === undefined ||
+    id === undefined
+  ) {
+    res
+      .status(400)
+      .send('name, age, profile_image_url, is_private & id are all required');
+    return;
+  }
+
+  const dbRes = await client.query(
+    'UPDATE candidate set name = $1, age = $2, profile_image_url = $3, is_private = $4 where id = $5',
+    [name, age, profile_image_url, is_private, id],
+  );
+  res.send({ data: `${dbRes.rowCount} row(s) updated`, status: 'success' });
+});
+
 app.listen(port);
 console.log(`app listening on ${port}`);
