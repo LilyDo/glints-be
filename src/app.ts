5d2360bc975704f31048704c96152fc08ca0bbac
Lily Do <dothuthao1504@gmail.com>
2022-10-02 20:51:41 +0800

    implement create profile endpoint and try catch

--- src/app.ts
+++ src/app.ts
@@ -17,17 +17,42 @@ async function connect() {
 
 connect();
 
-// GET candidate name, age method route
+// GET candidate details
 app.get('/candidate/:id', async (req, res) => {
   const dbRes = await client.query(
-    'SELECT id, name, age from candidate where id = $1',
+    'SELECT id, name, age, profile_image_url from candidate where id = $1',
     [req.params.id],
   );
 
   res.send({ data: dbRes.rows[0] || {}, status: 'success' });
 });
 
-// POST name, age, method route
+// create candidate details
+app.post('/candidate', async (req, res) => {
+  const { name, age, profile_image_url } = req.body;
+  if (
+    name === undefined ||
+    age === undefined ||
+    profile_image_url === undefined
+  ) {
+    res
+      .status(400)
+      .send('name, age, profile_image_url, is_private & id are all required');
+    return;
+  }
+  try {
+    const dbRes = await client.query(
+      'INSERT INTO candidate(name, age, profile_image_url, is_private) VALUES ($1, $2, $3, $4)',
+      [name, age, profile_image_url, false],
+    );
+    res.send({ data: `${dbRes.rowCount} row(s) created`, status: 'success' });
+  } catch (e) {
+    if (e) console.error(e);
+    res.status(500).send({ data: { message: e?.toString() }, status: 'error' });
+  }
+});
+
+// update candidate details
 app.post('/candidate/:id', async (req, res) => {
   const { name, age, profile_image_url, is_private } = req.body;
   const { id } = req.params;
@@ -35,8 +60,7 @@ app.post('/candidate/:id', async (req, res) => {
     name === undefined ||
     age === undefined ||
     profile_image_url === undefined ||
-    is_private === undefined ||
-    id === undefined
+    is_private === undefined
   ) {
     res
       .status(400)
@@ -44,11 +68,19 @@ app.post('/candidate/:id', async (req, res) => {
     return;
   }
 
-  const dbRes = await client.query(
-    'UPDATE candidate set name = $1, age = $2, profile_image_url = $3, is_private = $4 where id = $5',
-    [name, age, profile_image_url, is_private, id],
-  );
-  res.send({ data: `${dbRes.rowCount} row(s) updated`, status: 'success' });
+  try {
+    const dbRes = await client.query(
+      'UPDATE candidate set name = $1, age = $2, profile_image_url = $3, is_private = $4 where id = $5',
+      [name, age, profile_image_url, is_private, id],
+    );
+    res.send({
+      data: `${dbRes.rowCount} row(s) updated`,
+      status: 'success',
+    });
+  } catch (e) {
+    if (e) console.error(e);
+    res.status(500).send({ data: { message: e?.toString() }, status: 'error' });
+  }
 });
 
 app.listen(port);
